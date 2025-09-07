cb15ad904ae7e254bc3665876b0ab80223c5799b
amanraj953426 <amanraj953426@gmail.com>
2025-09-07 21:46:57 +0530

    Time: 4 ms (100%), Space: 43.8 MB (89.22%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -3,27 +3,25 @@ class Solution {
     public boolean isPalindrome(int x) {
         int num =x;
         int digit, rev=0;
+
+        // Negative number always not a Palendrome number
         if(x < 0){
             return false;
         }
 
 
         while(x!=0){
-            digit=x%10;
-            x=x/10;
-            rev=rev*10+digit;
+            digit=x%10; // take last digit
+            x=x/10; // remove last digit
+            rev=rev*10+digit; // reverse the number
         }
 
-
-
-        // boolean result;
 
         if(num == rev){
             return true;
         }else{
             return false;
         }
-        // return result;
     
     }
 }
