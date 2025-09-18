691a33a2405ce1eafeb2c9050de840df7fd38e30
amanraj953426 <amanraj953426@gmail.com>
2025-09-18 23:12:57 +0530

    Time: 5 ms (88.92%), Space: 44.7 MB (70.59%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -5,17 +5,13 @@ class Solution {
         int left = 0;
 
         Map<Character, Integer> mySet = new HashMap<>(); //Initialize HashMap
-        
-
         for(int right=0; right<s.length(); right++){
             char currentChar = s.charAt(right);
             if (mySet.containsKey(currentChar)){
                 left = Math.max(left, mySet.get(currentChar)+1);
-
             }
             mySet.put(currentChar, right);
             maxLength = Math.max(maxLength, right - left +1);
-
         }
         return maxLength;
 }
